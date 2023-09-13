b131c7be2d7ec16fbdd2c6d1312dafabbe30b5fb
Apoorva Dayal <at5139@srmist.edu.in>
2023-09-13 15:24:23 +0530

    SKAD-CHATBOT

---
+++ constants.js
@@ -0,0 +1,90 @@
+// Options the user could type in
+const prompts = [
+  ["popup"],
+  ["hi", "hey", "hello", "good morning", "good afternoon","i am from srm"],
+  ["how are you", "how is life", "how are things"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  [
+    "your name please",
+    "your name",
+    "may i know your name",
+    "what is your name",
+    "what call yourself"
+  ],
+  ["i love you"],
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+  ["bad", "bored", "tired"],
+  ["helps me", "tell me story", "tell me joke"],
+  ["ah", "yes", "ok", "okay", "nice"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  ["what should i eat today"],
+  ["bro"],
+  ["what", "why", "how", "where", "when"],
+  ["no","not sure","maybe","no thanks"],
+  [""],
+  ["haha","ha","lol","hehe","funny","joke"],
+  ["about"],
+  ["institute"],
+  ["admission process"],
+  ["contact details"],
+  ["course"],
+]
+
+// Possible responses, in corresponding order
+
+const replies = [
+  ["Welcome to International Admissions"],
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy","hi srmist","i want help from u"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  [
+    "Nothing much",
+    "About to go to sleep",
+    "Can you guess?",
+    "I don't know actually"
+  ],
+  ["I am infinite"],
+  ["I am just a bot", "I am a bot. What are you?"],
+  ["The one true God, JavaScript"],
+  ["I am nameless", "I don't have a name"],
+  ["I love you too", "Me too"],
+  ["Have you ever felt bad?", "Glad to hear it"],
+  ["Why?", "Why? You shouldn't!", "Try watching TV"],
+  ["What about?", "Once upon a time..."],
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+  ["Bye", "Goodbye", "See you later"],
+  ["Sushi", "Pizza"],
+  ["Bro!"],
+  ["Great question"],
+  ["That's ok","I understand","What do you want to talk about?"],
+  ["Please say something :("],
+  ["Haha!","Good one!"],
+  ["what you want to know"],
+  ["SRMIST is one of the top ranking and one of the most preferred institutions in India, established in 1985 by a visionary - Dr. T.R. Paarivendhar, Chancellor, SRM Institute of Science and Technology. It has four campuses, Kattankulathur, Ramapuram and Vadpalani in Chennai with the fourth campus at Modi Nagar,Ghaziabad and over 55,000 students and 3200 faculties are currently on rolls across all campuses."],
+  ["https://intlapplications.srmist.edu.in/"],
+  ["SRM Institute of Science and Technology( formerly known as SRM University )SRM Nagar, Kattankulathur,Chengalpattu District, Tamil Nadu603203"], 
+  ["Please choose the course you're interested in."]
+]
+
+// Random for any other user input
+
+// const alternative = [
+//   "sure which kind of help",
+//   "i am from srmist",
+//   "Same",
+//   "Go on...",
+//   "Bro...",
+//   "Try again",
+//   "I'm listening...",
+//   "I don't understand :/"
+// ]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
